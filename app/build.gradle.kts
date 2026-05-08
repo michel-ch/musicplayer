e069a3a4f9f801f9aef223d81abd3b53a4290e46
michel-ch <yepmtx@gmail.com>
2026-05-08 13:33:20 +0200

    chore(release): bump to v1.1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.musicplayer.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.1.8"
+        versionCode = 6
+        versionName = "1.1.9"
     }
 
     signingConfigs {
